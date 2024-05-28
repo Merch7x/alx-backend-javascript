651cd9aac7e10819797e711aa0139dd0005c27a2
Merch7x <aquamaingi@gmail.com>
2024-05-28 16:36:25 +0300

    Refctor method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,8 +3,9 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    getIncomeInDollars: () => `$${Object.values(budget)[0]}`,
-    getIncomeInEuros: () => `${Object.values(budget)[0]} euros`,
+    ...budget,
+    getIncomeInDollars: () => `$${budget.income}`,
+    getIncomeInEuros: () => `${budget.income} euros`,
   };
 
   return fullBudget;
