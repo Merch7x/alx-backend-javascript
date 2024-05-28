7fec9e7a9dcdb09abe161b45a62b4748c54b6b04
Merch7x <aquamaingi@gmail.com>
2024-05-28 09:53:24 +0300

    test

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,6 @@
 export default function concatArrays(array1, array2, string) {
-  const [...array3] = string;
+  // const [...array3] = string;\
+  const array3 = [string];
   const newarray = [...array1, ...array2, ...array3];
   return newarray;
 }
