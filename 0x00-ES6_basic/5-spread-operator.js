2c976d42e572ad335a61db68858125b1e8478a2f
Merch7x <aquamaingi@gmail.com>
2024-05-28 13:45:28 +0300

    Refactor excess code

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const [...array3] = string;
-  const newarray = [...array1, ...array2, ...array3];
-  return newarray;
+  return [...array1, ...array2, ...string]
 }
