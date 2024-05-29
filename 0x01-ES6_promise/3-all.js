4d2b6b34b26614e3372f550750fad5e27c7d569f
Merch7x <aquamaingi@gmail.com>
2024-05-30 01:07:45 +0300

    Handle multiple promises again

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const promise1 = createUser();
   const promise2 = uploadPhoto();
   Promise.all([promise1, promise2]).then((values) => {
-    console.log(values);
+    console.log(values[1].body, values[0].firstName, values[0].lastName);
   })
     .catch(() => {
       console.error('Signup system offline');
