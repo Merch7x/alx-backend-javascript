0a32e493b0e34cde34e3c16756841fd15e202c30
Merch7x <aquamaingi@gmail.com>
2024-05-30 01:21:20 +0300

    destructure values

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,12 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  const promise1 = createUser();
-  const promise2 = uploadPhoto();
-  Promise.all([promise1, promise2]).then((values) => {
-    console.log(values[1].body, values[0].firstName, values[0].lastName);
-  })
+  const userpromise = createUser();
+  const photopromise = uploadPhoto();
+  Promise.all([userpromise, photopromise])
+    .then(([user, photo]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
     .catch(() => {
       console.error('Signup system offline');
     });
