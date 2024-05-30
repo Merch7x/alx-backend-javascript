d87b3f612a30b52ba19a6c3532b105e0fd469d81
Merch7x <aquamaingi@gmail.com>
2024-05-30 21:52:56 +0300

    Return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   const userpromise = createUser();
   const photopromise = uploadPhoto();
-  Promise.all([userpromise, photopromise])
+  return Promise.all([userpromise, photopromise])
     .then(([user, photo]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
