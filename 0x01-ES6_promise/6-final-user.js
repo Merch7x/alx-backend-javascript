a8c35d0355411f7013ffe95d052fbbf8ab426100
Merch7x <aquamaingi@gmail.com>
2024-05-31 13:08:57 +0300

    Use async function'

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,28 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
+export default async function handleProfileSignup(firstname, lastname, filename) {
   const signpromise = signUpUser(firstname, lastname);
   const uploadpromise = uploadPhoto(filename);
 
-  return Promise.allSettled([signpromise, uploadpromise])
-    .then((results) => {
-      results.forEach((result) => {
-        if (result.status === 'fulfilled') {
-          console.log([
-            {
-              status: result.status,
-              value: result.value,
-            },
-          ]);
-        } else {
-          console.log([
-            {
-              status: result.status,
-              value: result.reason,
-            },
-          ]);
-        }
-      });
-    });
+  const results = await Promise.allSettled([signpromise, uploadpromise]);
+  return results.map((result) => (
+    [
+      {
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      },
+    ]
+  ));
 }
