c3a5fc1c24ad2d7289be1951ce2adf31e00cbbb8
Merch7x <aquamaingi@gmail.com>
2024-10-01 10:52:54 +0300

    Asycn file reading

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs/promises');
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(`${path}`, 'utf-8');
+    const lines = data.trim().split('\n');
+    const headers = lines[0].split(',');
+
+    const results = lines.slice(1).map(line => {
+      const values = line.split(',')
+      const obj = {};
+      headers.forEach((header, index) => {
+        obj[header.trim()] = values[index].trim();
+      });
+      return obj;
+    })
+    const fieldMap = {};
+
+    results.forEach(student => {
+      const field = student.field;
+      if (!fieldMap[field]) {
+        fieldMap[field] = { count: 0, names: [] };
+      }
+      fieldMap[field].count++;
+      fieldMap[field].names.push(student.firstname);
+    });
+    console.log(`Number of Students: ${results.length}`)
+
+    for (const [field, { count, names }] of Object.entries(fieldMap)) {
+      console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
+    }
+
+  } catch (err) {
+    console.error('Cannot load the database');
+  }
+
+}
+
+
+module.exports = countStudents;
