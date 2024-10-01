b0aca9b495ae8db2b9f5419d28549933b1fe2ef3
Merch7x <aquamaingi@gmail.com>
2024-10-01 19:45:26 +0800

    Lint code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,10 +4,11 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (input) => {
   process.stdout.write(`Your name is: ${input}`);
-  console.log('This important software is now closing')
-  process.exit()
+  console.log('This important software is now closing');
+  process.exit();
 });
 
 process.on('SIGINT', () => {
+  console.log('This important software is now closing');
   process.exit(0);
 });
