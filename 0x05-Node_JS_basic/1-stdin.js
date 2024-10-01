c7fcf259bdbbe7052312240c1eef36b4a293d65c
Merch7x <aquamaingi@gmail.com>
2024-10-01 08:43:02 +0300

    Work with process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input}`);
+  console.log('This important software is now closing')
+  process.exit();
+});
