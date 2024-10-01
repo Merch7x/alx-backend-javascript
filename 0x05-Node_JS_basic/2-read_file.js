66f41f56bbe5acf5099b05b7f1bf1647530e14db
Merch7x <aquamaingi@gmail.com>
2024-10-01 20:11:33 +0800

    Read from a file async and sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(path) {
   try {
@@ -6,35 +6,32 @@ function countStudents(path) {
     const lines = data.trim().split('\n');
     const headers = lines[0].split(',');
 
-    const results = lines.slice(1).map(line => {
-      const values = line.split(',')
+    const results = lines.slice(1).map((line) => {
+      const values = line.split(',');
       const obj = {};
       headers.forEach((header, index) => {
         obj[header.trim()] = values[index].trim();
       });
       return obj;
-    })
+    });
     const fieldMap = {};
 
-    results.forEach(student => {
-      const field = student.field;
+    results.forEach((student) => {
+      const { field } = student;
       if (!fieldMap[field]) {
         fieldMap[field] = { count: 0, names: [] };
       }
-      fieldMap[field].count++;
+      fieldMap[field].count += 1;
       fieldMap[field].names.push(student.firstname);
     });
-    console.log(`Number of Students: ${results.length}`)
+    console.log(`Number of Students: ${results.length}`);
 
     for (const [field, { count, names }] of Object.entries(fieldMap)) {
       console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
     }
-
   } catch (err) {
-    console.error('Cannot load the database')
+    console.error('Cannot load the database');
   }
-
 }
-
 
 module.exports = countStudents;
